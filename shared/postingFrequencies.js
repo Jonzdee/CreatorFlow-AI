91cdd0e2ccbc550404bcd163a0ed1517033a5c45
Jonzdee <ogunyankinjohnson@gmail.com>
2026-08-15 02:57:30 +0100

    update shared posting frequencies

--- shared/postingFrequencies.js
+++ shared/postingFrequencies.js
@@ -1,11 +1,19 @@
 export const POSTING_FREQUENCIES = [
-    "Daily",
-    "3 Times Weekly",
-    "Weekly",
+    {
+        id: "Daily",
+        title: "Daily",
+        description: "Post every day",
+    },
+    {
+        id: "3 Times Weekly",
+        title: "3 Times Weekly",
+        description: "Three posts every week",
+    },
+    {
+        id: "Weekly",
+        title: "Weekly",
+        description: "One post every week",
+    },
 ];
 
-export const POSTING_FREQUENCY_OPTIONS =
-    POSTING_FREQUENCIES.map((item) => ({
-        id: item,
-        title: item,
-    }));
+export const POSTING_FREQUENCY_OPTIONS = POSTING_FREQUENCIES;
