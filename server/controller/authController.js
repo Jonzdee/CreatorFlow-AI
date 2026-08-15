98d876446ee5de009757c408199b8d8b6b0aed29
Jonzdee <ogunyankinjohnson@gmail.com>
2026-08-15 03:28:02 +0100

    completion of onboarding

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -41,7 +41,8 @@ export const registerUser = async (req, res) => {
                 id: user._id,
                 name: user.name,
                 email: user.email,
-                niche: user.niche
+                niche: user.niche,
+                onboardingCompleted: user.onboardingCompleted
             }
         });
     } catch (error) {
@@ -100,13 +101,10 @@ export const loginUser = async (req, res) => {
             user: {
 
                 id: user._id,
-
                 name: user.name,
-
                 email: user.email,
-
-                niche: user.niche
-
+                niche: user.niche,
+                onboardingCompleted: user.onboardingCompleted,
             }
 
         });
